9e1a92a6362f7c8b636fa4139467ecd5ac11e974
Sumit Meena <sumitmeena4562@gmail.com>
2026-03-02 23:38:14 +0530

    fix: correct axios import path in VendorInvoice.jsx

--- client/src/components/Vendors/Dashboard/Orders/VendorInvoice.jsx
+++ client/src/components/Vendors/Dashboard/Orders/VendorInvoice.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import api from '../../../../../api/axios';
+import api from '../../../../api/axios';
 
 const VendorInvoice = () => {
     const { id } = useParams();
